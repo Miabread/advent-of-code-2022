e6c283780db94d3ec95ecd25c15d1bbf3de8e2d9
JamesBeeProg <JamesBeeProg@gmail.com>
2022-12-05 08:31:42 -0500

    Setup project

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::fs::read_to_string;
+
+mod day1;
+
+fn main() {
+    let day = std::env::args()
+        .nth(1)
+        .expect("no day given")
+        .parse()
+        .expect("to be a number");
+
+    let main = match day {
+        1 => day1::main,
+        _ => panic!("not a valid day"),
+    };
+
+    let input = read_to_string(format!("./input/day{day}.txt")).expect("input file should exist");
+
+    main(&input);
+}
